434073ad7ceede08eaa78461bb764712d75fce2c
JadeBoumaza <jade.boumaza@laposte.net>
2014-12-30 14:22:28 +0100

    Create Signout.java
    servlet pour la déconnexion

---
+++ Signout.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package intergiciels.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Jade BOUMAZA
+ *
+ */
+public class Signout extends HttpServlet {
+	
+	public static final String URL_REDIRECTION = "/applier/signin";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+        /* Récupération et destruction de la session en cours */
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        /* Redirection vers la page de connexion */
+        response.sendRedirect(URL_REDIRECTION);
+    }
+
+}
