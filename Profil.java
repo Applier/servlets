3bd70daae0fd66f42e0ce86ef92983d345df4ed1
JadeBoumaza <jade.boumaza@laposte.net>
2015-01-02 11:34:12 +0100

    Update and rename FormulaireCV.java to Profil.java

--- Profil.java
+++ Profil.java
@@ -8,34 +8,51 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import intergiciels.beans.User;
 import intergiciels.beans.InfosCV;
-import intergiciels.forms.FormulaireCVForm;
+import intergiciels.forms.ProfilForm;
 
 /**
  * @author Jade BOUMAZA
  *
  */
-public class FormulaireCV extends HttpServlet {
-	public static final String VUE = "/WEB-INF/formulairecv.jsp";
-	public static final String ATTRIBUT_INFOSCV = "infosCV";
-	public static final String ATTRIBUT_FORM = "form";
+public class Profil extends HttpServlet {
+	public static final String VUE = "/WEB-INF/profil.jsp";
+	public static final String ATTRIBUT_USER = "user";
+    public static final String ATTRIBUT_FORM = "form";
+    public static final String ATTRIBUT_SESSIONUSER = "sessionUser";
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* Affichage de la page de formulaire du CV */
+		/* Affichage de la page de profil */
 		this.getServletContext().getRequestDispatcher(VUE).forward(request,response);
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		/* Préparation de l'objet formulaire */
-		FormulaireCVForm form = new FormulaireCVForm();
+		ProfilForm form = new ProfilForm();
 		
 		/* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
-		InfosCV infosCV = form.remplirInfosCV(request);
+		InfosCV infos = form.remplirInfosCV(request);
+		
+		/* Récupération de la session depuis la requête */
+		HttpSession session = request.getSession();
+		
+		/* Récupération du bean User de la session en cours */
+		User user = (User) session.getAttribute(ATTRIBUT_SESSIONUSER);
+		
+		/* Ajout des données du profil aux infos de l'user */
+		user.setInfos(infos);
+		
+		/* Ajout du bean User à la session */
+		session.setAttribute(ATTRIBUT_SESSIONUSER, user);
 		
 		/* Stockage du formulaire et du bean dans l'objet request */
 		request.setAttribute(ATTRIBUT_FORM, form);
-		request.setAttribute(ATTRIBUT_INFOSCV, infosCV);
+		request.setAttribute(ATTRIBUT_USER, user);
+		
 		
 		this.getServletContext().getRequestDispatcher(VUE).forward(request,response);
 	}
